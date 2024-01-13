35c03415dc69a5030108f31786bbd494dd7b3317
Jiaye <jiaye.wei@epfl.ch>
2024-01-13 15:35:12 +0100

    Modify the student class

--- src/builder/objects/student.py
+++ src/builder/objects/student.py
@@ -1,28 +1,19 @@
 class Student:
-    def __init__(self, name, student_id):
+    def __init__(self, name, sciper, degree):
         self.name = name
-        self.student_id = student_id
+        self.sciper = sciper
+        self.degree = degree
 
     def display_info(self):
         print(f"Name: {self.name}\nStudent ID: {self.student_id}")
 
 
 class BachelorStudent(Student):
-    def __init__(self, name, student_id, major):
-        super().__init__(name, student_id)
-        self.major = major
-
-    def display_info(self):
-        super().display_info()
-        print(f"Major: {self.major}\nStudent Type: Bachelor")
+    def __init__(self, name, sciper):
+        super().__init__(name, sciper, 'bachelor')
 
 
 class MasterStudent(Student):
-    def __init__(self, name, student_id, thesis_topic):
-        super().__init__(name, student_id)
-        self.thesis_topic = thesis_topic
-
-    def display_info(self):
-        super().display_info()
-        print(f"Thesis Topic: {self.thesis_topic}\nStudent Type: Master")
+    def __init__(self, name, sciper):
+        super().__init__(name, sciper, 'master')
 
