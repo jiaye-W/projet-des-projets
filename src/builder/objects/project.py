5de9e51e7da0aeb90fe0191f7063f19ef2d12691
Jiaye <jiaye.wei@epfl.ch>
2024-01-13 15:47:12 +0100

    Add class of project initializer

---
+++ src/builder/objects/project.py
@@ -0,0 +1,13 @@
+"""
+The class of projects
+
+Created on: 13.01.2024
+
+@author: Jiaye Wei <jiaye.wei@epfl.ch>
+"""
+
+class Project:
+    def __init__(self, title, description, type):
+        self.title = title
+        self.description = description
+        self.type = type
