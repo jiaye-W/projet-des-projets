2c1fc9104592cc587684092260cdb66d57be997d
Jiaye <jiaye.wei@epfl.ch>
2024-01-14 16:58:56 +0100

    Add subclass pseudo-project, which contains project objects for group-based projects

--- src/builder/objects/project.py
+++ src/builder/objects/project.py
@@ -7,7 +7,13 @@ Created on: 13.01.2024
 """
 
 class Project:
-    def __init__(self, title, description, type):
+    def __init__(self, supervisor, title, description, stud_type):
+        self.supervisor = supervisor
         self.title = title
         self.description = description
-        self.type = type
+        self.stud_type = stud_type # bachelor/master/both
+
+
+class PseudoProject(Project):
+    def __init__(self, supervisor, stud_type, title=None, description=None):
+        super().__init__(supervisor, stud_type, title, description)
