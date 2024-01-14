e14d331de0676da62068c28d91bdbf5345575be6
Jiaye <jiaye.wei@epfl.ch>
2024-01-14 23:35:04 +0100

    Move the chairs data as a global data in test script. Get the research_areas and chairs from it. Refactor codes.

--- tests/factory.py
+++ tests/factory.py
@@ -8,11 +8,22 @@ Created on: 13.01.2024
 
 import random
 
-from src.builder.objects.project import Project
-from src.builder.objects.student import Student
+# All chairs at SMA
+# source: https://www.epfl.ch/schools/sb/research/math/research/
+research = {"Algebra and Number Theory": ["ARG", "CAG", "EGG", "ERG", "GR-TES", "TAN", "TN"],
+            "Analysis": ["AMCV", "CSFT", "DOLA", "PDE", "TAN", "CMGR", "ERG", "SCI-SB-JS", "PROPDE"],
+            "Data Science and Learning": ["ANCHP", "BIOSTAT", "CSFT", "DOLA", "Deparis-Group", "LCVMM", "MCSS", "MDS", "OPTIM", "SDS", "SMAT", "STAT"],
+            "Discrete Mathematics": ["DISOPT", "ERG", "MDS"],
+            "Geometry and Topology": ["ARG", "CAG", "EGG", "CMGR", "GR-TR", "UPHESS"],
+            "Mathematics in Computational Sciences": ["ANCHP", "CSQI", "Deparis-Group", "GR-PI", "LCVMM", "MCSS", "MNS", "OPTIM"],
+            "Probability and Statistics": ["CSFT", "PROB", "PRST", "RGM", "SMAT", "STOAN", "PROPDE"],
+            "Statistics": ["BIOSTAT", "DOLA", "MDS", "OPTIM", "SDS", "SMAT", "STAT"]}
+
+research_areas = list(research.keys())
+chairs = list(set(value for values_list in research.values() for value in values_list))
 
 def test():
-    students = [("Alice", "bachelor"),
+    students = [("Alice", "bachelor", ["Geometry and Topology"]),
                 ("")]
 
 def generate_students():
@@ -21,12 +32,5 @@ def generate_students():
     random.choice()
 
 def generate_supervisors():
-    # All chairs at SMA
-    chairs = {"Algebra and Number Theory": ["ARG", "CAG", "EGG", "ERG", "GR-TES", "TAN", "TN"],
-              "Analysis": ["AMCV", "CSFT", "DOLA", "PDE", "TAN", "CMGR", "ERG", "SCI-SB-JS", "PROPDE"],
-              "Data Science and Learning": ["ANCHP", "BIOSTAT", "CSFT", "DOLA", "Deparis-Group", "LCVMM", "MCSS", "MDS", "OPTIM", "SDS", "SMAT", "STAT"],
-              "Discrete Mathematics": ["DISOPT", "ERG", "MDS"],
-              "Geometry and Topology": ["ARG", "CAG", "EGG", "CMGR", "GR-TR", "UPHESS"],
-              "Mathematics in Computational Sciences": ["ANCHP", "CSQI", "Deparis-Group", "GR-PI", "LCVMM", "MCSS", "MNS", "OPTIM"],
-              "Probability and Statistics": ["CSFT", "PROB", "PRST", "RGM", "SMAT", "STOAN", "PROPDE"],
-              "Statistics": ["BIOSTAT", "DOLA", "MDS", "OPTIM", "SDS", "SMAT", "STAT"]}
+    return 0
+
