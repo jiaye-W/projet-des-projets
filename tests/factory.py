24aead49a43b6f5585db87b7166a8452b7dc7a35
Jiaye <jiaye.wei@epfl.ch>
2024-01-17 22:17:12 +0100

    Add methods of getting the project-supervisors dict, from the group-based raw data

--- tests/factory.py
+++ tests/factory.py
@@ -33,15 +33,43 @@ chairs = list(set(value for values_list in research.values() for value in values
     - mandatory capacity
     - bachelor obligation
 """
-projects_group_based = {"ARG": (3, 2), "CAG": (4, 2), "TN": (3, 1), 
-                        "DOLA": (3, 2), "PDE": (5, 3), "SCI-SB-JS": (4, 1),
-                        "ANCHP": (6, 4), "CSFT": (4, 3), "MDS": (3, 2),
-                        "DISOPT": (4, 1),
-                        "UPHESS": (3, 2),
-                        "CSQI": (4, 2), "MCSS": (5, 2),
-                        "PROB": (4, 1), "PROPDE": (3, 2), 
-                        "BIOSTAT": (3, 1), "SMAT": (4, 2)}
-number_of_projects_group_based = sum(value[0] for value in projects_group_based.values())
+# The raw data from the Google form
+data_group_based = {"ARG": (3, 2), "CAG": (4, 2), "TN": (3, 1), 
+                    "DOLA": (3, 2), "PDE": (5, 3), "SCI-SB-JS": (4, 1),
+                    "ANCHP": (6, 4), "CSFT": (4, 3), "MDS": (3, 2),
+                    "DISOPT": (4, 1),
+                    "UPHESS": (3, 2),
+                    "CSQI": (4, 2), "MCSS": (5, 2),
+                    "PROB": (4, 1), "PROPDE": (3, 2), 
+                    "BIOSTAT": (3, 1), "SMAT": (4, 2)}
+number_of_projects_group_based = sum(value[0] for value in data_group_based.values())
+# naming of project of group-based projects: "ARG-1", "ARG-2", "ARG-3", "CAG-1", etc
+# also have to contain the degree requirement: "ARG-1", 
+
+def get_group_based_project_supervisors(data_group_based):
+    """_summary_
+
+    Args:
+        data_group_based (_type_): raw data gathering from Google forms
+
+    Returns:
+        _type_: _description_
+    """
+    group_based_project_supervisors = {}
+
+    for chair in data_group_based:
+        # read the pair of numbers: (# of projects, # of projects for only masters)
+        (number_of_projects, number_of_projects_only_masters) = data_group_based[chair]
+
+        for i in range(1, number_of_projects+1):
+            if (i <= number_of_projects_only_masters):
+                group_based_project_supervisors[f"{chair}-proj-{i} (only master)"] = chair
+            else:
+                group_based_project_supervisors[f"{chair}-proj-{i}"] = chair
+
+    return group_based_project_supervisors
+
+# Build up projects from dict 
 
 """Project-based projects
 
@@ -109,4 +137,5 @@ def generate_degree_of_student(random, probability_of_master):
         return "bachelor"
 
 if __name__ == "__main__":
-    generate_students(number_of_students)
+    #generate_students(number_of_students)
+    get_group_based_project_supervisors(data_group_based)
