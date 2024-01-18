7da362bc91624eb09e90d96da6db81f90deaa507
Jiaye <jiaye.wei@epfl.ch>
2024-01-18 23:48:24 +0100

    Small changes on student class

--- src/builder/objects/student.py
+++ src/builder/objects/student.py
@@ -1,19 +1,15 @@
 class Student:
-    def __init__(self, name, sciper, degree):
-        self.name = name
-        self.sciper = sciper
+    def __init__(self, name, degree, research_interest):
+        self.name = name # or any equivalent identity of student (sciper, email, etc)
         self.degree = degree
-
-    def display_info(self):
-        print(f"Name: {self.name}\nStudent ID: {self.student_id}")
-
+        self.research_interests = research_interest # a list 
 
 class BachelorStudent(Student):
-    def __init__(self, name, sciper):
-        super().__init__(name, sciper, 'bachelor')
+    def __init__(self, name, research_interest):
+        super().__init__(name, 'bachelor', research_interest)
 
 
 class MasterStudent(Student):
-    def __init__(self, name, sciper):
-        super().__init__(name, sciper, 'master')
+    def __init__(self, name, research_interest):
+        super().__init__(name, 'master', research_interest)
 
