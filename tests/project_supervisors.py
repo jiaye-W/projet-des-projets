a67eab8e5e2e2494961bb89da0c3f3816f4477c1
Jiaye <jiaye.wei@epfl.ch>
2024-01-20 00:48:24 +0100

    Refactor proj_sup slightly

--- tests/project_supervisors.py
+++ tests/project_supervisors.py
@@ -9,20 +9,11 @@
     - mandatory capacity
     - bachelor obligation
 """
-# The raw data from the Google form
-data_group_based = {"ARG": (3, 2), "CAG": (4, 2), "TN": (3, 1), 
-                    "DOLA": (3, 2), "PDE": (5, 3), "SCI-SB-JS": (4, 1),
-                    "ANCHP": (6, 4), "CSFT": (4, 3), "MDS": (3, 2),
-                    "DISOPT": (4, 1),
-                    "UPHESS": (3, 2),
-                    "CSQI": (4, 2), "MCSS": (5, 2),
-                    "PROB": (4, 1), "PROPDE": (3, 2), 
-                    "BIOSTAT": (3, 1), "SMAT": (4, 2)}
-number_of_projects_group_based = sum(value[0] for value in data_group_based.values())
+
 # naming of project of group-based projects: "ARG-1", "ARG-2", "ARG-3", "CAG-1", etc
 # also have to contain the degree requirement: "ARG-1", 
 
-def get_project_supervisors(data_group_based, data_project_based):
+def build_project_supervisors(data_group_based, data_project_based):
     """_summary_
 
     Args:
@@ -38,15 +29,17 @@ def get_project_supervisors(data_group_based, data_project_based):
     project_supervisors = {**group_based_project_supervisors, **project_based_project_supervisors}
     return project_supervisors
 
+def get_group_based_project_capacities(data_group_based):
+    return 0
 
 def get_group_based_project_supervisors(data_group_based):
-    """_summary_
+    """Get the data of project-supervisors correspondence, for group-based projects
 
     Args:
-        data_group_based (_type_): raw data gathering from Google forms
+        data_group_based(dict): raw data gathered from Google forms for group-based projects
 
     Returns:
-        _type_: _description_
+        project_supervisors: correspondence between projects and supervisors
     """
     group_based_project_supervisors = {}
 
@@ -56,9 +49,9 @@ def get_group_based_project_supervisors(data_group_based):
 
         for i in range(1, number_of_projects+1):
             if (i <= number_of_projects_only_masters):
-                group_based_project_supervisors[f"{chair}-proj-{i} (only master)"] = chair
+                group_based_project_supervisors[f"{chair}, proj-{i} (only master)"] = chair
             else:
-                group_based_project_supervisors[f"{chair}-proj-{i}"] = chair
+                group_based_project_supervisors[f"{chair}, proj-{i}"] = chair
 
     return group_based_project_supervisors
 
@@ -73,17 +66,21 @@ def get_group_based_project_supervisors(data_group_based):
     - mandatory capacity
     - bachelor obligation
 """
-projects_project_based = {"EGG": [(1, "proj1-EGG", "bachelor"), (2, "proj2-EGG", "master")], 
-                          "ERG": [(1, "proj1-ERG", "master"), (2, "proj2-ERG", "master"), (3, "proj3-ERG", "bachelor"), (4, "proj4-ERG", "master")]}
-number_of_projects_project_based = sum(len(value) for value in projects_project_based.values())
+data_project_based = {"EGG": [(1, "proj1-EGG", "bachelor"), (2, "proj2-EGG", "master")], 
+                      "ERG": [(1, "proj1-ERG", "master"), (2, "proj2-ERG", "master"), (3, "proj3-ERG", "bachelor"), (4, "proj4-ERG", "master")]}
+number_of_projects_project_based = sum(len(value) for value in data_project_based.values())
 
 def get_project_based_project_supervisors(data_project_based):
-    """_summary_
+    """Get the data of project-supervisors correspondence, for project-based projects
+
+    Args:
+        data_project_based(dict): raw data gathered from Google forms
 
     Returns:
-        _type_: _description_
+        project_supervisors: correspondence between projects and supervisors
     """
     project_based_project_supervisors = {}
 
-    return project_based_project_supervisors
 
+
+    return project_based_project_supervisors
