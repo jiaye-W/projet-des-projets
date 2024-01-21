d9d17177b6bbea0c50dd771ed915f9862d733dc0
Jiaye <jiaye.wei@epfl.ch>
2024-01-21 17:09:16 +0100

    Color the matching results in the same bipartite graph

--- tests/graph.py
+++ tests/graph.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from main import number_of_students
 from main import project_supervisors
 from main import student_preferences
+from main import matching
 
 # Create a bipartite graph
 G = nx.Graph()
@@ -28,15 +29,20 @@ pos = nx.bipartite_layout(G,
 
 fig, ax = plt.subplots(figsize=(6, 30))
 
+# Color the matching results
+matching_edges = [(f'{stud}', f'{proj}') for proj, students_matched in matching.items() for stud in students_matched]
+
 # Draw the bipartite graph
 nx.draw(G, 
-        pos=pos, 
-        with_labels=True, 
-        font_weight='bold', 
+        pos = pos, 
+        with_labels =True, 
+        font_weight = 'bold', 
         font_size = 4,
-        node_color=['skyblue']*number_of_students + ['lightcoral']*len(list_of_projects), 
+        node_color = ['skyblue']*number_of_students + ['lightcoral']*len(list_of_projects), 
         node_size = 50,
-        ax=ax)
+        edge_color = ['red' if (f'{edge[0]}', f'{edge[1]}') in matching_edges else 'black' for edge in G.edges()],
+        ax = ax)
 
-plt.savefig("plot.png", dpi=1000)
+plt.title('Stable matching results')
+plt.savefig("playground/plot.png", dpi=1000)
 plt.clf()
