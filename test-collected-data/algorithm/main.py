9fc4d0ce45fb8102ba135148e0c8fe8d55b22ca6
Jiaye <jiaye.wei@epfl.ch>
2024-07-16 23:48:17 +0200

    Finish the test of algorithm in main.py and write stats into a file

--- test-collected-data/algorithm/main.py
+++ test-collected-data/algorithm/main.py
@@ -62,29 +62,92 @@ all_dictionaries = {
 # Writing dictionaries to the file
 write_dictionaries_to_file('results.txt', all_dictionaries)
 
-# print("Missing supervisors for student projects:", missing_supervisors)
-# print("Added students to supervisors' preference lists:")
-# for student, project in added_to_supervisors:
-#     print(f"{student} chose {project}, and was added to {project.split('-')[1]}'s preference list")
+def get_ordinal_suffix(number):
+    if 10 <= number % 100 <= 20:
+        suffix = 'th'
+    else:
+        suffix = {1: 'st', 2: 'nd', 3: 'rd'}.get(number % 10, 'th')
+    return suffix
 
-# print("\nUpdated supervisor preferences:")
-# for supervisor, prefs in supervisor_preferences.items():
-#     print(f"{supervisor}: {prefs}")
+class CustomDictPrinter(dict):
+    def __str__(self):
+        return '\n'.join([f'{key}: {value}' for key, value in self.items()])
 
-game = StudentAllocation.create_from_dictionaries(
-    student_preferences,
-    supervisor_preferences,
-    project_supervisors,
-    project_capacities,
-    supervisor_capacities)
+def apply_matching_algorithm(student_preferences,
+                             supervisor_preferences,
+                             project_supervisors,
+                             project_capacities,
+                             supervisor_capacities):
+    """Apply matching algorithm"""
+    game = StudentAllocation.create_from_dictionaries(
+        student_preferences,
+        supervisor_preferences,
+        project_supervisors,
+        project_capacities,
+        supervisor_capacities)
 
-matching = student_allocation(game.students, game.projects, game.supervisors, optimal='student')
+    matching = student_allocation(game.students, game.projects, game.supervisors)
 
-# result matching
-print(len(student_preferences.keys()))
+    """Extract information of the matching results"""
+    # Get the number of students being matched
+    number_of_students = len(student_preferences.keys())
 
-print_dict_items(matching)
+    students_matched = set()
+    for studs in matching.values():
+        students_matched.update(studs)
 
-# the size of matching
-matching_size = sum(1 for value in matching.values() if value)
-print(f"The size of the matching = {matching_size}")
+    students_unmatched = set(game.students).difference(students_matched)
+    students_unmatched = list(students_unmatched)
+    students_matched = list(students_matched)
+
+    #matching_edges = [(stud, str(proj)) for proj, students_matched in matching.items() for stud in students_matched]
+
+    with open('solution.txt', 'w') as file:
+    # Redirect the output to the file
+        print(f'There are {len(students_matched)} students got matched, out of {number_of_students}\n', file=file)
+
+        print(f'Unmatched student(s): {", ".join(map(str, students_unmatched))}' if students_unmatched
+              else 'Every students are matched!', 
+              file=file)
+
+        """Print out matching results"""
+        print('\nThe student-project correspondence: ', file=file)
+
+        number_of_students_got_first_pick = 0
+        number_of_students_got_second_pick = 0
+        number_of_students_got_third_pick = 0
+
+        matching_stud_proj = {stud: proj for proj, studs in matching.items() for stud in studs}
+        for stud in matching_stud_proj:
+            proj = matching_stud_proj[stud]
+            prefs_list = student_preferences[f'{stud}']
+            which_pick = prefs_list.index(f'{proj}')+1
+
+            if which_pick == 1:
+                number_of_students_got_first_pick += 1
+            if which_pick == 2:
+                number_of_students_got_second_pick += 1
+            if which_pick == 3:
+                number_of_students_got_third_pick += 1
+
+            print(f'{stud} : {proj}, his/her {which_pick}{get_ordinal_suffix(which_pick)} choice', file=file)
+        
+        print(f'\nNumber of students got matched = {len(students_matched)} ({round(len(students_matched) * 100 / number_of_students, 2)}%)', file=file)
+        print(f'Number of students got matched to their 1st choice = {number_of_students_got_first_pick} ({round(number_of_students_got_first_pick * 100 / number_of_students, 2)}%)', file=file)
+        print(f'Number of students got matched to their 2nd choice = {number_of_students_got_second_pick} ({round(number_of_students_got_second_pick * 100 / number_of_students, 2)}%)', file=file)
+        print(f'Number of students got matched to their 3rd choice = {number_of_students_got_third_pick} ({round(number_of_students_got_third_pick * 100 / number_of_students, 2)}%)', file=file)
+
+        file.write('\n' + '-' * 100 + '\n')
+
+        print('\nThe project-students correspondence: ', file=file)
+        matching_proj_studs = {proj: studs for proj, studs in matching.items() if studs}
+        print(CustomDictPrinter(matching_proj_studs), file=file)
+
+        file.write('\n' + '-' * 100 + '\n')
+
+if __name__ == '__main__':
+    apply_matching_algorithm(student_preferences, 
+                             supervisor_preferences, 
+                             project_supervisors, 
+                             project_capacities, 
+                             supervisor_capacities)
