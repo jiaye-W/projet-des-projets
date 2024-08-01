bbe6d469618b1ae2519a9414930166c649c865fb
Jiaye <jiaye.wei@epfl.ch>
2024-08-01 15:01:07 +0800

    Finish build_student_preferences and retrive student_info for publishing matching results

--- src/actual_run/builder.py
+++ src/actual_run/builder.py
@@ -147,9 +147,9 @@ def build_project_supervisors(df):
     
 def classify_pair(first, second):
     if not math.isnan(first):
-        return f"Project-based-{int(first)}"
+        return f"Project-{int(first)}"
     elif not math.isnan(second):
-        return f"Group-based-{int(second)}"
+        return f"Group-{int(second)}"
     else:
         return "Both values are nan"
     
@@ -157,12 +157,16 @@ def build_student_preferences(df):
     """Build the dict student_preferences from the dataframe of df_student
 
     Args:
-        df (dataframe): _description_
+        df (dataframe): dataframe of student csv
 
     Returns:
-        _type_: _description_
+        student_preferences (dict)
+        student_info (dict)
+            - key: "Student-{id}"
+            - value: student dataclass object
     """
     student_preferences = {}
+    student_info = {}
 
     for _, row in df.iterrows():
 
@@ -179,12 +183,12 @@ def build_student_preferences(df):
             last_name=row['Last Name'],
             email=row['Email'],
             sciper=row['Sciper'],
-            master=row['Master'],
-            first_choice=first_choice,
-            second_choice=second_choice,
-            third_choice=third_choice,
-            fourth_choice=fourth_choice,
-            fifth_choice=fifth_choice
+            master=row['Master']
+            # first_choice=first_choice,
+            # second_choice=second_choice,
+            # third_choice=third_choice,
+            # fourth_choice=fourth_choice,
+            # fifth_choice=fifth_choice
         )
 
         student_preferences[f"Student-{id}"] = [first_choice, 
@@ -193,7 +197,9 @@ def build_student_preferences(df):
                                                 fourth_choice,
                                                 fifth_choice]
         
-    return student_preferences
+        student_info[f"Student-{id}"] = student
+        
+    return student_preferences, student_info
 
 def get_information_from_results(dict):
     """From the matching results (dict), get the information of 
@@ -207,8 +213,6 @@ def get_information_from_results(dict):
         _type_: _description_
     """
 
-
-
     return 0
 
 if __name__ == '__main__':
@@ -221,12 +225,14 @@ if __name__ == '__main__':
     # df_student = download_and_process_csv(url_student)
 
     df_supervisor_1 = pd.read_csv('src/actual_run/data/supervisor-1.csv')
+    df_student = pd.read_csv('src/actual_run/data/student.csv')
     
-    print(df_supervisor_1)
+    # print(df_supervisor_1)
     # print(df_student)
 
     # Build up dicts and writefile
     project_supervisors, project_capacities, supervisor_capacities, real_project_supervisor, project_title = build_project_supervisors(df_supervisor_1)
+    student_preferences, student_info = build_student_preferences(df_student)
     with open('src/actual_run/results/algo_inputs.txt', 'w') as file:
         file.write('-' * 100 + '\n')
         file.write("Project-Supervisors\n\n")
@@ -243,6 +249,11 @@ if __name__ == '__main__':
         for key, value in supervisor_capacities.items():
             file.write(f"{key}: {value}\n")
 
+        file.write('\n' + '-' * 100 + '\n')
+        file.write("Student-Preferences\n\n")
+        for key, value in student_preferences.items():
+            file.write(f"{key}: {value}\n")
+
     with open('src/actual_run/results/project_supervisor_correspondence.txt', 'w') as file:
         for key, value in real_project_supervisor.items():
             file.write(f"{key}: {value}\n")
@@ -251,3 +262,6 @@ if __name__ == '__main__':
         for key, value in project_title.items():
             file.write(f"{key}: {value}\n")
     
+    with open('src/actual_run/results/students_information.txt', 'w') as file:
+        for key, value in student_info.items():
+            file.write(f"{key}: {value}\n")
