c01b0b0b4900c5da9b8e3f3a0a28802b8f334fc0
Jiaye <jiaye.wei@epfl.ch>
2024-08-09 18:39:44 +0800

    Add a main file for the final execution
    For now we have added part of the documentation

---
+++ src/actual_run/main.py
@@ -0,0 +1,26 @@
+"""
+Date: 31.07.2024
+
+Eventually, the objects are:
+    - Project-based projects
+        E.g., Project-17 represents the project-based project of ID=17
+    - Group-based (pseudo)projects
+        E.g., Group-11 represents the group-based pseudo-project of ID=11
+     
+------------------------------------------------------------------------------------
+Dictionaries for the matching algorithm:
+
+1st Supervisor Form
+    - project_supervisors
+    - project_capacities
+    - supervisor_capacities
+
+Student Form
+    - student_preferences
+
+2nd Supervisor Form
+    - supervisor_preferences
+
+------------------------------------------------------------------------------------
+
+"""
